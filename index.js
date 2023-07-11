c8395fa58a448b170c972f3f518936259e3c966f
BreElle Wells <breellejordyn@gmail.com>
2023-07-10 21:37:11 -0600

    complete functions lab

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(streetNumber) {
+    if (streetNumber >= 42) {
+        return streetNumber - 42;
+    }
+    else {
+        return 42 - streetNumber;
+    }
+}
+
+function distanceFromHqInFeet(streetNumber) {
+    return distanceFromHqInBlocks(streetNumber) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+    }
+    else if (distance <= 2000) {
+        return (distance - 400) * .02;
+    }
+    else if (distance <= 2500) {
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+}
+
+// describe('calculatesFarePrice(start, destination)', function() {
+//     it('gives customers a free sample', function() {
+//       expect(calculatesFarePrice(43, 44)).to.equal(0);
+//     });
+
+//     it('charges 2 cents per foot when total feet travelled is between 400 and 2000 (remember the first 400 feet are free!)', function() {
+//       // total feet travelled is 528, minus the first 400 feet, comes to 128 -> 2.56
+//       expect(calculatesFarePrice(34, 32)).to.equal(2.56);
+//     });
+
+//     it('charges 25 dollars for a distance over 2000 feet', function() {
+//       expect(calculatesFarePrice(50, 58)).to.equal(25);
+//     });
+
+//     it('does not allow rides over 2500 feet', function() {
+//       expect(calculatesFarePrice(34, 24)).to.equal('cannot travel that far');
+//     });
+//   });
